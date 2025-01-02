e483ced420fea4c041458a6add5ef95151234bda
Naveen.K <139785502+NaveenMadMax@users.noreply.github.com>
2025-01-02 23:14:28 +0530

    completed practice on react Routes

--- React/ReactProject/src/App.tsx
+++ React/ReactProject/src/App.tsx
@@ -1,13 +1,22 @@
+import './App.css';
+import Navbar from './Components/Navbar/NavBar'
+import Home from  './pages/Home'
+import Products from './pages/Products'
+import AboutUs from './pages/AboutUs'
+import FillForm from './pages/FillForm'
+import {Routes, Route} from 'react-router-dom'
 
-import Task10 from './Components/Task10/task10';
-import './App.css'
 const App= () => {
   return (
-    <div className='displayName'>
-      <h1>Fill this From</h1>
-      <Task10 />
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/aboutus" element={<AboutUs/>} />
+        <Route path="/fillform" element={<FillForm/>} />
+      </Routes>
     </div>
   );
-};
-
+}
 export default App;
---
+++ React/ReactProject/src/pages/FillForm.tsx
@@ -0,0 +1,11 @@
+import Task10 from "./task10"
+
+const FillForm = () => {
+  return (
+    <div>
+      <Task10/>
+    </div>
+  )
+}
+
+export default FillForm
