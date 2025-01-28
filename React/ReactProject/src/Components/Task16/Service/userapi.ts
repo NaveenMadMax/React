f50fe9ee7402af64fa76d1de56a29c2010963e12
Naveen.K <139785502+NaveenMadMax@users.noreply.github.com>
2025-01-28 19:36:54 +0530

    completed TASK16 with Project Structure

--- React/ReactProject/src/Components/Task16/Service/userapi.ts
+++ React/ReactProject/src/Components/Task16/Service/userapi.ts
@@ -1,12 +1,11 @@
 
 import { User } from "../Entity/user";
 import axios from "axios";
-const baseURL = "https://randomuser.me/api/";
 
 export function fetchData():Promise<User>{
  return new Promise(async(resolve, reject)=>{
     try{
-        let response = await axios.get (baseURL);
+        let response = await axios.get ("https://randomuser.me/api/");
         let fetchedUser = response.data.results?.[0];
         resolve(fetchedUser);
     }catch (error){
